13f7741e5764af843addc66b8f685e71fee7ffef
Matías Santurio <matiassantuu@gmail.com>
2022-12-04 14:56:08 -0300

    Day four :)

---
+++ 4.2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+class Main_4_2 {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new FileReader("4.in"));
+        var line = br.readLine();
+        var total = 0;
+        while (line != null) {
+            var arr = line.split(",");
+            String[] fst = arr[0].split("-"), snd = arr[1].split("-");
+            int fstfst = Integer.parseInt(fst[0]), fstsnd = Integer.parseInt(fst[1]);
+            int sndfst = Integer.parseInt(snd[0]), sndsnd = Integer.parseInt(snd[1]);
+            if ((fstfst <= sndfst && fstsnd >= sndfst) || (sndfst <= fstfst && sndsnd >= fstfst) ||
+                    (fstfst <= sndsnd && fstsnd >= sndsnd) || (sndfst <= fstsnd && sndsnd >= fstsnd)) {
+                total++;
+            }
+            line = br.readLine();
+        }
+        System.out.println("The sum of the priorities is: " + total);
+        br.close();
+    }
+}
